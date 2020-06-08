3b1c24f2dee6dab9a801224ea8d7452d2f95d0b1
Chris Nokes <nokesc@yahoo.com>
2020-06-07 23:44:42 -0500

    dbeaver

---
+++ apt-install/dbeaver.sh
@@ -0,0 +1,4 @@
+wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt-get update && sudo apt-get install dbeaver-ce
+echo end
